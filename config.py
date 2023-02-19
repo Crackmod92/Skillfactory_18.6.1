3be015464f47bb5f5a26fd69dec31cfb2e5a4ec8
Crackmod <crackmod92@gmail.com>
2023-02-19 18:20:29 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 TOKEN = '5990869428:AAG56JK_Q_f_VxfEyokZndk0ZBkl4klLxzU'
 
-currency = {'доллар': 'USD',
+keys = {'доллар': 'USD',
         'евро': 'EUR',
         'рубль': 'RUB',
         'лира': 'TRY',
